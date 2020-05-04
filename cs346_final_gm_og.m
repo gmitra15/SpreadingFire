5960826bd3051cc34d3560b1e3f5d7cf5ae3d738
Owen <owengoldthwaite@gmail.com>
2020-05-03 20:31:13 -0400

    New fire conditions, wind, and initial fire boundary variables

--- cs346_final_gm_og.m
+++ cs346_final_gm_og.m
@@ -8,9 +8,6 @@
 %
 % To run: cs346_final_gm_og.m
 
-clear
-close all
-
 %% Simulation Parameters %%%
 
 % Seed the random number generator for testing; saves rng settings to a var
@@ -22,8 +19,8 @@ simLength = 35; % length of simulation
 numIterations = 1 + simLength/dt;
 
 % Grid dimensions
-rows = 100; % width
-cols = 100; % length
+row_count = 100; % width
+col_count = 100; % length
 
 %% Constants %%
 DIRT = 1; % Dirt cell that doesn't burn
@@ -33,22 +30,47 @@ FIRE = 3; % Tree cell that is on fire
 prob_init_tree = 0.7; % initial probability a cell is a tree
 prob_init_fire = 0.025; % initial probability a tree is on fire
 
-% chance that a tree exposed to fire from N/S/E/W ignites
-prob_catch_fire_cardinal = 0.44; 
+% Percent increase of fire to occur for each N/E/S/W tile thats on fire,
+% e.g. 3 of them on fire = 3*cardinal_fire_chance_increase
+cardinal_fire_chance_increase = 0.35;
 
-% chance that a tree  exposed to fire on a diagonal (i.e. NE/NW/SE/SW) ignites
-prob_catch_fire_diag = 0.25;
+% Same thing as cardinal increase but for diagonal
+diag_fire_chance_increase = 0.25;
 
 prob_lightning = 0.0005; % probability that a cell spontaneously ignites
 
+% Wind constants set here, 1 is default. Currently just hard coded in so use
+% realistic values, i.e if N_wind is high then S_wind should be something 
+% relatively small
+% Also for this wind a North wind blows the fire towards the north, south towards
+% south, East towards east, and west towards west. Not the inverse.
+N_wind = 1/3;
+E_wind = 1;
+S_wind = 2;
+W_wind = 1;
+
+card_wind_speeds = [N_wind, W_wind, S_wind, E_wind];
+diag_wind_speeds = [N_wind * W_wind, S_wind * W_wind, N_wind * E_wind, S_wind * E_wind];
+
+% Boundary values for where to spawn fire, only spawns initially within these 
+% values
+fire_row_upper = 100;
+fire_row_lower = 70;
+fire_col_lower = 0;
+fire_col_upper = 100;
+
 %% Set up forest grid
 % Initialize forest to be all dirt
-forests = ones(rows, cols, numIterations) * DIRT;
-for row = 1:rows
-    for col = 1:cols
+forests = ones(row_count, col_count, numIterations) * DIRT;
+for row = 1:row_count
+    for col = 1:col_count
         if rand < prob_init_tree % Randomly add trees
             if rand < prob_init_fire % Set a percentage of the trees to be lit
-                forests(row, col, 1) = FIRE;
+                % Making fire spawn within fire bounds
+                if( (row > fire_row_lower && row < fire_row_upper)... 
+                &&  (col > fire_col_lower && row < fire_col_upper) )
+                    forests(row, col, 1) = FIRE;
+                end
             else
                 forests(row, col, 1) = TREE;
             end
@@ -70,8 +92,9 @@ for frame = 2:numIterations
     
     %% Loop for updating each cell in the grid
     % Loop over the indices corresponding to the original (non-extended) grid
-    for row = 2:rows + 1
-        for col = 2:cols + 1
+    for row = 2:row_count + 1
+        for col = 2:col_count + 1
+        
             % Von Neumann Neighborhood
             grid_point  = extended_forest(row, col);
             north = extended_forest(row - 1, col);
@@ -84,7 +107,7 @@ for frame = 2:numIterations
             southeast  = extended_forest(row + 1, col + 1);
             northwest = extended_forest(row - 1, col - 1);
             southwest  = extended_forest(row + 1, col - 1);
-            
+
             %% Update cell
             % If the cell was dirt, it stays dirt
             if grid_point == DIRT
@@ -96,28 +119,37 @@ for frame = 2:numIterations
 
             % If the cell was a tree, set it on fire or leave it alone
             elseif grid_point == TREE
+
+                % Making arrays of the direction values
+                card_dirs = [north, east, south, west];
+                diag_dirs = [northeast, southeast, northwest, southwest];
                 
-                % If a neighboring cell was on fire, it is exposed
-                % Any cell exposed to fire has a probability of catching fire
-                if (north==FIRE || east==FIRE || south==FIRE || west==FIRE || ... 
-                        northeast==FIRE || southeast==FIRE || ...
-                        northwest==FIRE || southwest==FIRE)
-                    % i think it would be cooler if each adjacent (N/E/S/W) tile on
-                    % fire adds to the likelihood that tree becomes on
-                    % fire, and each diagonal tile adds a smaller
-                    % percentage
-                    if (rand < prob_catch_fire_cardinal) 
-                        updated_grid_point = FIRE;
-                    else
-                        updated_grid_point = TREE;
-                    end
-                    
-                % Lightning strikes
-                elseif (rand < prob_lightning)
+                % Logical fun to see how many neighbors are on fire then
+                % multiplying number by chance of new fire
+                card_fire_chance = sum((card_dirs == FIRE)...
+                                        * cardinal_fire_chance_increase...
+                                        .* card_wind_speeds);
+
+                diag_fire_chance = sum((diag_dirs == FIRE)...
+                                        * diag_fire_chance_increase...
+                                        .* diag_wind_speeds);
+                
+                fire_chance = card_fire_chance + diag_fire_chance;
+
+                if (rand < fire_chance) 
                     updated_grid_point = FIRE;
-                    
                 else
                     updated_grid_point = TREE;
+                end
+                    
+                % Lightning strikes
+                if (rand < prob_lightning)
+                    updated_grid_point = FIRE;
+                else
+                    % Doesnt change cuz not struck by lighting, this is here
+                    % Because we dont want it to get set to TREE incase it's
+                    % already on fire
+                    updated_grid_point = updated_grid_point;
                 end
             end
             % Place the updated cell into the forests grid
@@ -147,22 +179,22 @@ axis equal;
 hold on;
 
 % Add a button that lets you end the program early
-tb = axtoolbar(viz_axes,{'zoomin','zoomout','rotate','restoreview'});
-btn = axtoolbarbtn(tb,'state');
+% tb = axtoolbar(viz_axes,{'zoomin','zoomout','rotate','restoreview'});
+% btn = axtoolbarbtn(tb,'state');
 
 disp("Drawing...");
 for i = 1:numIterations
     
     % End the animation early if you press the square button to the left of the
     % magnifying glass
-    stop_state = get(btn, 'Value');
-    if stop_state
-       close(gcf);
-       break;
-    end
+    % stop_state = get(btn, 'Value');
+    % if stop_state
+    %    close(gcf);
+    %    break;
+    % end
     
     % Turn each forest grid into an image
     image(viz_axes, forests(:, :, i));
-    pause(0.7);
+    pause(0.4);
 end
 disp("Simulation complete!");
