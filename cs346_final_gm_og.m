9e75ce8c090708d550644c5f0aba2bca04ee516b
Owen <owengoldthwaite@gmail.com>
2020-05-08 12:45:30 -0400

    Added rain movement yeehaw

--- cs346_final_gm_og.m
+++ cs346_final_gm_og.m
@@ -15,7 +15,7 @@ rng_set = rng(1);
 
 % Time-related variables
 dt = 1; % timestep
-simLength = 70; % length of simulation
+simLength = 101; % length of simulation
 numIterations = 1 + simLength/dt;
 
 % Grid dimensions
@@ -36,7 +36,8 @@ prob_init_grass = 0.6; % initial probability a cell is grass
 prob_init_fire = 0.025; % initial probability a tree is on fire
 
 % number of timesteps it takes for cloud to shift one position
-cloud_move_const = 2; % movement constant for rain clouds, higher is slower
+cloud_move_const = 1; % movement constant for rain clouds, higher is slower
+rain_move_speed = 1; % Amount of cells rain can mover per timestep
 
 % Time values
 initial_tree_time = 8;
@@ -57,15 +58,15 @@ prob_lightning = 0.00005; % probability that a cell spontaneously ignites
 % Wind constants set here, 1 is default. Currently just hard coded in so use
 % realistic values, i.e if N_wind is high then S_wind should be something 
 % relatively small
-% Also for this wind a North wind blows the fire towards the north, south towards
-% south, East towards east, and west towards west. Not the inverse.
-N_wind = 1/5;
-E_wind = 5;
-S_wind = 5;
-W_wind = 1/5;
+% Wind direction is what you would use if you were sailing / explaining the
+% weather, i.e. a south wind comes from the south and therefore moves north
+N_wind = 1;
+E_wind = 1;
+S_wind = 1;
+W_wind = 1;
 
-card_wind_speeds = [S_wind, E_wind, N_wind, W_wind];
-diag_wind_speeds = [S_wind * E_wind, N_wind * E_wind, S_wind * W_wind, N_wind * W_wind];
+card_wind_speeds = [S_wind, W_wind, N_wind, E_wind];
+diag_wind_speeds = [S_wind * W_wind, N_wind * W_wind, S_wind * E_wind, N_wind * E_wind];
 
 % Boundary values for where to spawn fire, only spawns initially within these 
 % values
@@ -76,10 +77,13 @@ fire_col_upper = 100;
 
 % Boundary values for where to spawn rain, only spawns initially within these 
 % values
-rain_row_upper = 30;
-rain_row_lower = 0;
-rain_col_lower = 0;
+rain_row_lower = 1;
+rain_row_upper = 35;
+rain_col_lower = 1;
 rain_col_upper = 100;
+
+% List of rain boundaries that gets changed/used by rain movement
+boundary_list = [rain_row_lower, rain_row_upper, rain_col_lower, rain_col_upper];
 
 %% Set up forest grid
 % Initialize forest to be all dirt
@@ -125,15 +129,30 @@ for frame = 2:numIterations
     % Create a grid thats the size of the forest + 2 on each side
     extended_grid_size = size(forests( : , : , frame-1))+2;
     extended_forest = ones(extended_grid_size) * DIRT; % initialize all as dirt
-    extented_rain_grid = ones(extended_grid_size) * DRY;
+    extended_rain_grid = ones(extended_grid_size) * DRY;
     
     % Set the inside portion of the grid equal to the forest values from
     % the previous timestep (a.k.a the previous frame)
     extended_forest(2:end-1, 2:end-1) = forests(:,:,frame-1);
-    extented_rain_grid((2:end-1, 2:end-1)) = rain_grids(:,:,frame-1)
-
+    extended_rain_grid(2:end-1, 2:end-1) = rain_grids(:,:,frame-1);
+    
     time_grid = time_grids(:,:,frame-1);
-    rain_grid = rain_grids(:,:,frame-1);
+    
+    % Rain cloud movement based on the wind
+    if(mod(frame, cloud_move_const) == 0)
+        % Getting wind direction
+        [val, wind_dir] = max([card_wind_speeds, diag_wind_speeds]);
+
+        % Updating the rain grid
+        [updated_rain_grid, b_list] = update_rain(wind_dir, extended_grid_size, boundary_list, RAIN, rain_move_speed);
+        % Updating the current boundaries of the rain area
+        boundary_list = b_list;
+    else
+        % If rain didnt update this time step
+        updated_rain_grid = extended_rain_grid;
+    end
+    rain_grids(:, :, frame) = updated_rain_grid(2:end-1, 2:end-1);
+    
     %% Loop for updating each cell in the grid
     % Loop over the indices corresponding to the original (non-extended) grid
     for row = 2:row_count + 1
@@ -144,15 +163,15 @@ for frame = 2:numIterations
             time_grid_point = time_grid(row-1, col-1);
 
             north = extended_forest(row - 1, col);
-            east  = extended_forest(row, col + 1);
+            east  = extended_forest(row, col - 1);
             south = extended_forest(row + 1, col);
-            west  = extended_forest(row, col - 1);
+            west  = extended_forest(row, col + 1);
             
             % Additional code for Moore neighborhood
-            northeast = extended_forest(row - 1, col + 1);
-            southeast  = extended_forest(row + 1, col + 1);
-            northwest = extended_forest(row - 1, col - 1);
-            southwest  = extended_forest(row + 1, col - 1);
+            northeast = extended_forest(row - 1, col - 1);
+            southeast  = extended_forest(row + 1, col - 1);
+            northwest = extended_forest(row - 1, col + 1);
+            southwest  = extended_forest(row + 1, col + 1);
 
             %% Update cell
             % If the cell was dirt, it stays dirt
@@ -211,20 +230,7 @@ for frame = 2:numIterations
             % forest grid being pushed down and to the right by one unit
             % from the extension
 
-            % Rain movement
-            rain_grid_point = rain_grid(row, col);
-            updated_rain_point = rain_grid_point;
-
-            if(mod(frame, cloud_move_const) == 0)
-                val, idx = max(card_wind_speeds);
-
-                if(idx == 1)
-                    rain_grid(row - 1, col) = RAIN;
-                    updated_rain_point = DRY;
-
-
-            end
-
+            rain_grid_point = rain_grids(row-1, col-1, frame);
             % Rain putting out fires
             is_raining = rain_grid_point == RAIN; % if it is raining on the cell
             if(updated_grid_point == FIRE && is_raining) % if on fire
@@ -240,7 +246,6 @@ for frame = 2:numIterations
             
             forests(row - 1, col - 1, frame) = updated_grid_point;
             time_grids(row - 1, col - 1, frame) = time_grid_point;
-            rain_grids(row - 1, col - 1, frame) = rain_grid_point;
         end
     end
 end
@@ -264,8 +269,8 @@ axis off;
 axis equal;
 hold on;
 
-rain_fig = figure;
-rain_axes = axes(rain_fig);
+%rain_fig = figure;
+rain_axes = axes(viz_fig);
 
 rain_color = [0, 0, 1];
 dry_color  = [1, 1, 1];
@@ -277,10 +282,127 @@ hold on;
 
 disp("Drawing...");
 for i = 1:numIterations
-   
+    
     % Turn each forest grid into an image
-    image(viz_axes, forests(:, :, i));
-    image(rain_axes, rain_grids(:, :, i));
-    pause(0.2);
+    im1 = image(viz_axes, forests(:, :, i));
+    im2 = image(rain_axes, rain_grids(:, :, i), "AlphaData",.01);
+
+    pause(0.05);
 end
 disp("Simulation complete!");
+
+function [updated_rain_grid, boundary_list] = update_rain(wind_direction, grid_size, boundary_list, RAIN, move_rate)
+    updated_rain_grid = ones(grid_size);
+    rain_row_lower = boundary_list(1);
+    rain_row_upper = boundary_list(2);
+    rain_col_lower = boundary_list(3);
+    rain_col_upper = boundary_list(4);
+    row_count = grid_size(1);
+    col_count = grid_size(2);
+
+    if(wind_direction == 1) % South wind, moving north
+        if(rain_row_upper + move_rate <= row_count)
+            rain_row_upper = rain_row_upper + move_rate;
+        end
+        if(rain_row_lower + move_rate <= row_count)
+            rain_row_lower = rain_row_lower + move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+
+    if(wind_direction == 2) % West wind, moving east
+        if(rain_col_upper + move_rate <= col_count)
+            rain_col_upper = rain_col_upper + move_rate;
+        end
+        if(rain_col_lower + move_rate <= col_count)
+            rain_col_lower = rain_col_lower + move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+
+    if(wind_direction == 3) % North wind, moving south
+        if(rain_row_upper - move_rate >= 1)
+            rain_row_upper = rain_row_upper - move_rate;
+        end
+        if(rain_row_lower - move_rate >= 1)
+            rain_row_lower = rain_row_lower - move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+    
+    if(wind_direction == 4) % East wind, moving west
+        if(rain_col_upper - move_rate >= 1)
+            rain_col_upper = rain_col_upper - move_rate;
+        end
+        if(rain_col_lower - move_rate >= 1)
+            rain_col_lower = rain_col_lower - move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+
+    if(wind_direction == 5) % South West wind, moving North East
+        if(rain_row_upper + move_rate <= row_count)
+            rain_row_upper = rain_row_upper + move_rate;
+        end
+        if(rain_row_lower + move_rate <= row_count)
+            rain_row_lower = rain_row_lower + move_rate;
+        end
+        if(rain_col_upper + move_rate <= col_count)
+            rain_col_upper = rain_col_upper + move_rate;
+        end
+        if(rain_col_lower + move_rate <= col_count)
+            rain_col_lower = rain_col_lower + move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+
+    if(wind_direction == 6) % North West wind, moving South East
+        if(rain_row_upper - move_rate >= 1)
+            rain_row_upper = rain_row_upper - move_rate;
+        end
+        if(rain_row_lower - move_rate >= 1)
+            rain_row_lower = rain_row_lower - move_rate;
+        end
+        if(rain_col_upper + move_rate <= col_count)
+            rain_col_upper = rain_col_upper + move_rate;
+        end
+        if(rain_col_lower + move_rate <= col_count)
+            rain_col_lower = rain_col_lower + move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+
+    if(wind_direction == 7) % South East wind, moving North West
+        if(rain_row_upper + move_rate <= row_count)
+            rain_row_upper = rain_row_upper + move_rate;
+        end
+        if(rain_row_lower + move_rate <= row_count)
+            rain_row_lower = rain_row_lower + move_rate;
+        end
+        if(rain_col_upper - move_rate >= 1)
+            rain_col_upper = rain_col_upper - move_rate;
+        end
+        if(rain_col_lower - move_rate >= 1)
+            rain_col_lower = rain_col_lower - move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+
+    if(wind_direction == 8) % North East wind, moving South West
+        if(rain_row_upper - move_rate >= 1)
+            rain_row_upper = rain_row_upper - move_rate;
+        end
+        if(rain_row_lower - move_rate >= 1)
+            rain_row_lower = rain_row_lower - move_rate;
+        end
+        if(rain_col_upper - move_rate >= 1)
+            rain_col_upper = rain_col_upper - move_rate;
+        end
+        if(rain_col_lower - move_rate >= 1)
+            rain_col_lower = rain_col_lower - move_rate;
+        end
+        updated_rain_grid(rain_row_lower:rain_row_upper, rain_col_lower:rain_col_upper) = RAIN;
+    end
+    
+    boundary_list = [rain_row_lower, rain_row_upper, rain_col_lower, rain_col_upper];
+end
